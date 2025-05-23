169beed8672be0ab20e100c8078ab1df6570a441
Redesco Progetti srl - G. Notaro <bjack795@gmail.com>
2025-05-23 13:25:34 +0200

    Aggiornato base path per GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/VerbaleIspezione/', // Nome corretto del repository
+  base: '/VerbaleIspezione/', // Nome del repository
   server: {
     host: '0.0.0.0',
     port: 5173,
